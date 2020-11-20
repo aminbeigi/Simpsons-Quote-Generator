aed55e05ca51ccc93f225195390e7afa2acc9b08
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-20 13:22:32 +1100

    Move contents of eventlistener into function

--- src/script.js
+++ src/script.js
@@ -8,22 +8,36 @@ const fetchData = async () => {
 
 const generateQuoteBtn = document.querySelector('[data-generate-quote]');
 
-generateQuoteBtn.addEventListener('click', () => {
+const loadRandomQuote = async () => {
     const quoteElement = document.querySelector('[data-quote]');
     const quoteCharacterElement = document.querySelector('[data-character]');
     const quoteImageElement = document.querySelector('[data-quote-image]');
+    const quoteLoaderElement = document.querySelector('[data-loader]');
     if (!quoteElement || !quoteCharacterElement || !quoteImageElement) {
         throw new Error('Missing Elements');
     }
 
     quoteElement.innerHTML = 'Loading...';
-    //quoteImageElement.style.display = 'none';
+    quoteCharacterElement.innerHTML = '';
+
+    quoteImageElement.style.display = 'none';
+    quoteLoaderElement.style.display = 'block';
+    
 
     const promise = fetchData();
     promise.then((data) => {
             quoteElement.innerHTML = data[0]['quote'];
             quoteCharacterElement.innerHTML = data[0]['character'];
+
+            quoteImageElement.style.display = 'block';
+            quoteLoaderElement.style.display = 'none';
+
             quoteImageElement.src = data[0]['image'];
         }
     )
-})
+}
+
+// on page load load content
+loadRandomQuote();
+
+generateQuoteBtn.addEventListener('click', loadRandomQuote);
