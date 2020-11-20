72f6c3c7a683c6acab905ef6904608ee0266564c
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-20 11:14:26 +1100

    Clean up code

--- src/script.js
+++ src/script.js
@@ -6,24 +6,26 @@ const fetchData = async (url) => {
     request.open('GET', url, false);
     request.send(null);
     jsonResponse = JSON.parse(request.responseText);
-    return new Promise((resolve) => { resolve(jsonResponse)});
+    return jsonResponse;
 }
 
 const generateQuoteBtn = document.querySelector('[data-generate-quote]');
-
 
 generateQuoteBtn.addEventListener('click', () => {
     const quoteElement = document.querySelector('[data-quote]');
     const quoteCharacterElement = document.querySelector('[data-character]');
     const quoteImageElement = document.querySelector('[data-quote-image]');
     if (!quoteElement || !quoteCharacterElement || !quoteImageElement) {
-        throw new Error('Missing Elements')
+        throw new Error('Missing Elements');
     }
 
-    fetchData(API_URL).then((data) => {
-            quoteElement.innerHTML = data[0]['quote'];  
+    quoteElement.innerHTML = 'Loading...';
+
+    const promise = fetchData(API_URL);
+    promise.then((data) => {
+            quoteElement.innerHTML = data[0]['quote'];
             quoteCharacterElement.innerHTML = data[0]['character'];
-            quoteImageElement.src = data[0]['image']
+            quoteImageElement.src = data[0]['image'];
         }
     )
 })
